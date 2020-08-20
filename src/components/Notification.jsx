6b698ba3947cacc1d1f0d7d188232afe3c098c07
qiuhao <qiuhao951@gmail.com>
2020-08-20 15:28:34 +0800

    clear warning

--- src/components/Notification.jsx
+++ src/components/Notification.jsx
@@ -1,5 +1,4 @@
-import React, { useState, useCallback } from 'react';
-import clsx from 'clsx';
+import React  from 'react';
 import classes from './Notification.module.css';
 import { Container } from '../ui-components/Container';
 import { ReactComponent as CloseIcon } from '../assets/close.svg';
@@ -8,7 +7,7 @@ export const Notification = ({ onClose }) => {
     return (
         <div className={classes.root}>
             <Container>
-                <p className={classes.p1}>There is NO OFFICIAL PUBLIC SALE of Acala Tokens. BEWARE OF SCAMS & REPORT THEM TO THE <a href='mailto:hello@acala.network' target='_blank'>ACALA TEAM</a></p>
+                <p className={classes.p1}>There is NO OFFICIAL PUBLIC SALE of Acala Tokens. BEWARE OF SCAMS & REPORT THEM TO THE <a href='mailto:hello@acala.network' target='_blank' rel="noopener noreferrer">ACALA TEAM</a></p>
             </Container>
             <div onClick={onClose} className={classes.close}><CloseIcon /></div>
         </div>

4b3cb2e85daeb3abe6cbf52d7fccff08d765f21c
qiuhao <qiuhao951@gmail.com>
2020-08-27 15:19:14 +0800

    update partners

--- src/components/Partners.jsx
+++ src/components/Partners.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import classes from './Partners.module.css';
+
 import polychain from '../assets/polychain.gif';
 import kr1 from '../assets/kr1.svg';
 import p2pCaptital from '../assets/p2p-capital.svg';
@@ -12,13 +13,24 @@ import web3Foundation from '../assets/web3-foundation.svg';
 import p2pValidator from '../assets/p-2-p-validator.svg';
 import hypersphere from '../assets/hypersphere.svg';
 import berkeley from '../assets/berkeley.svg';
+import pantera from '../assets/pantera.jpg';
+import arrington from '../assets/arrington-xrp-capital.svg';
 import digitalRenaissance from '../assets/digitalRenaissance.svg';
+import parafi from '../assets/parafi.svg';
+import s1confirmation from '../assets/1confirmation.svg';
+import spartan from '../assets/spartan.jpg';
+import altonomy from '../assets/altonomy.svg';
+import CMS from '../assets/CMS.png';
+import divergence from '../assets/divergence.svg';
+import coinfund from '../assets/coinfund.png';
+import dcg from '../assets/dcg.png';
+
 import { Container } from '../ui-components/Container';
 import { ShowAnimation } from '../ui-components/ShowAnimation';
 
-const ListItem = React.forwardRef(({ name, img, link, onClick }, ref) => (
+const ListItem = React.forwardRef(({ name, img, link, onClick, style }, ref) => (
     <li className={classes.item} ref={ref} onClick={() => { onClick(link) }}>
-        <img src={img} alt={name} className={classes[name]} />
+        <img src={img} alt={name} className={classes[name]} style={style} />
     </li>
 ));
 const List = ({ list }) => {
@@ -51,7 +63,17 @@ export const Partners = () => {
         { name: 'berkeley', img: berkeley, link: 'https://www.xcelerator.berkeley.edu' },
         { name: 'drf', img: digitalRenaissance, link: 'https://drf.ee' },
         { name: 'p2pValidator', img: p2pValidator, link: 'https://p2p.org' },
-        { name: 'hypersphere', img: hypersphere, link: 'https://www.hypersphere.ventures' }
+        { name: 'hypersphere', img: hypersphere, link: 'https://www.hypersphere.ventures' },
+        { name: 'pantera', img: pantera, link: 'https://www.panteracapital.com' },
+        { name: '1confirmation', img: s1confirmation, link: 'https://www.1confirmation.com' },
+        { name: 'arrington', img: arrington, link: 'http://arringtonxrpcapital.com', style: { maxWidth: '60%' } },
+        { name: 'parafi', img: parafi, link: 'https://www.parafi.capital' },
+        { name: 'spartan', img: spartan, link: 'https://www.spartangroup.io' },
+        { name: 'altonomy', img: altonomy, link: 'https://www.altonomy.com' },
+        { name: 'CMS', img: CMS, link: '' },
+        { name: 'divergence', img: divergence, link: '' },
+        { name: 'coinfund', img: coinfund, link: 'https://coinfund.io' },
+        { name: 'dcg',  img: dcg, link: 'https://dcg.co', style: { background: '#000' } }
     ];
 
     return (
